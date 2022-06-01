33ed73ac3f5dffbb071a5d76f43f7c17d932145b
Mateo <miniatimat@gmail.com>
2022-06-01 20:12:44 -0300

    Falta Test

--- api/index.js
+++ api/index.js
@@ -21,9 +21,11 @@ const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 const {Dog} = require("./src/db")
 
+const port = process.env.PORT || 3001
+
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  server.listen(3001, () => {
+  server.listen(port, () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
     /*Dog.create({
       name: "Test",
